cf50a713d87b5810b75a71010f4d6a1a803045bf
Aditya <aditya@amandjaja.com>
2025-03-04 09:53:20 +0700

    initialize module

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Keda Tech Test',
+    'version' : '1.1',
+    'summary': 'Keda Tech Test',
+    'sequence': 1,
+    'description': """""",
+    'depends' : ['base_setup'],
+    'data': [],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'license': 'LGPL-3',
+}
