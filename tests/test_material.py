3d476f712b1eb0e9ec423815cde876627e4019dd
Aditya <aditya@amandjaja.com>
2025-03-05 13:33:24 +0700

    modif test case update

--- tests/test_material.py
+++ tests/test_material.py
@@ -1,4 +1,5 @@
-from odoo.tests import HttpCase, TransactionCase  
+from odoo.tests import HttpCase, TransactionCase
+import json
 import logging
 _logger = logging.getLogger(__name__)
 
@@ -7,14 +8,24 @@ class TestMaterial(HttpCase):
 
     # test connection
     def test_get_data(self):
+        self.authenticate("admin", "admin")
         response = self.url_open('/api/material')
+        _logger.info('n=== response class: %s ===' % type(response).__name__)
+        _logger.info('n=== response content: %s ===' % (response.content,))
         self.assertEqual(response.status_code, 200, 'I think "get data" it\'s not ok.')
     
     def test_update_data(self):
-        response = self.url_open('/api/material/update/1', data={
-            'name': 'ABC'
+        self.authenticate("admin", "admin")
+        post_data = json.dumps({
+            "name": "ABC"
         })
-        self.assertEqual(response.status_code, 200, 'I think "update data" it\'s not ok.')
+
+        response1 = self.url_open('/api/material/update/1', data=post_data, headers={'Content-Type': 'application/json'})
+        self.assertEqual(response1.status_code, 200, 'Result should be 200.')
+        if response1.status_code == 200:
+            res1_json = response1.json()
+            result1 = res1_json.get('result', {})
+            self.assertEqual(result1['status'], 200, 'Result should be 200. Err: %s' % result1.get('err', False))
 
     def test_remove_data(self):
         response = self.url_open('/api/material/remove/1', data={
