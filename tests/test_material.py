800e9c05e663272401d0c7f0832bf32d4bc6aa64
Aditya <aditya@amandjaja.com>
2025-03-05 10:30:51 +0700

    add test connection

--- tests/test_material.py
+++ tests/test_material.py
@@ -5,12 +5,19 @@ _logger = logging.getLogger(__name__)
 
 class TestMaterial(HttpCase):
 
+    # test connection
     def test_get_data(self):
         response = self.url_open('/api/material')
-        self.assertEqual(response.status_code, 200, 'I think "get data" it\'s ok.')
+        self.assertEqual(response.status_code, 200, 'I think "get data" it\'s not ok.')
     
     def test_update_data(self):
-        response = self.url_open('/api/material/update/11', data={
+        response = self.url_open('/api/material/update/1', data={
             'name': 'ABC'
         })
-        self.assertEqual(response.status_code, 200, 'I think "update data" it\'s ok.')
+        self.assertEqual(response.status_code, 200, 'I think "update data" it\'s not ok.')
+
+    def test_remove_data(self):
+        response = self.url_open('/api/material/remove/1', data={
+            'id': 1,
+        })
+        self.assertEqual(response.status_code, 200, 'I think "remove data" it\'s not ok.')
